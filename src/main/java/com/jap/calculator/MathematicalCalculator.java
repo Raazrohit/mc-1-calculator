3f0bc6c4808259529e8049bbd7d0570a314343f2
Rohit Raj <rohit2000lmp@gmail.com>
2022-08-25 09:41:32 +0530

    final submit

--- src/main/java/com/jap/calculator/MathematicalCalculator.java
+++ src/main/java/com/jap/calculator/MathematicalCalculator.java
@@ -3,25 +3,57 @@ package com.jap.calculator;
 public class MathematicalCalculator {
 
 	public int add(int num1, int num2)
-	{	
-		return -1;
+	{
+		int addition = 0;
+		try{
+			addition = num1+num2;
+		}catch (Exception e){
+			System.out.println("e = " + e);
+		}
+
+		return addition;
 	}
 	public int subtract(int num1, int num2)
-	{	
-		return -2;
+	{
+		int subtraction = 0;
+		try{
+			subtraction = num1-num2;
+		}catch(Exception e) {
+			System.out.println("e = " + e);
+		}
+		return subtraction;
 	}
 	public int multiply(int num1, int num2)
-	{	
-		return -1;
+	{
+		int multiplication = 0;
+		try{
+			multiplication = num1 * num2;
+		}catch(Exception e){
+			System.out.println("e = " + e);
+		}
+
+		return multiplication;
 	}
 	public int divide(int num1, int num2)
 	{
 		//write the code and handle the ArithmeticException
-         return -1;
+		int division = 0;
+		try{
+			division = num1/num2;
+		}catch(ArithmeticException e){
+			System.out.println("e = " + e);
+		}
+		return division;
 
 	}
 	public int modulo(int num1, int num2)
-	{	
-		return-1;
+	{
+		int module = 0;
+		try{
+			module = num1%num2;
+		}catch (Exception e){
+			System.out.println("e = " + e);
+		}
+		return module;
 	}
 }
--- src/main/java/com/jap/calculator/ScientificCalculator.java
+++ src/main/java/com/jap/calculator/ScientificCalculator.java
@@ -5,40 +5,70 @@ public class ScientificCalculator {
 	public double cielOfANumber(float num) throws CalculatorException
 	{
 		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
+		if(num <= 0){
+			throw new CalculatorException("number cannot be equal to or less than 0");
+		}else{
+			num = (int)num;
+		}
 		//or else call ceil method of Math class and return the value
 
-		return 1;
+		return num;
 	}
-	
+
 	public double floorOfANumber(float num) throws CalculatorException
 	{
 		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
+
+		if(num <= 0){
+			throw new CalculatorException("number cannot be equal to or less than 0");
+		}else{
+			num = (int)num + 1;
+		}
 		//or else call floor method of Math class and return the value
-		return 1;
+		return num;
 	}
-	
-    public long power(int num1, int num2) throws CalculatorException {
+
+	public long power(int num1, int num2) throws CalculatorException {
 		//if num1 is less than num2 than throw the userdefined exception with proper message
+		long power =0;
+		if(num1 < num2){
+			throw new CalculatorException("number1 cannot be less than number2 ");
+		}else{
+			power = (long) Math.pow(num1,num2);
+		}
 		//else use Math pow method and return the value
 
-		return -1;
-    }
-    
-    public double squareRoot(int num) throws CalculatorException
-    {
-		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
-			//or else call sqrt method of Math class and return the value
+		return power;
+	}
 
-		return -1;
-    }
+	public double squareRoot(int num) throws CalculatorException
+	{
+		double number =0;
+		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
+		if(num <= 0){
+			throw new CalculatorException("number cannot be equal to or less than 0");
+		}else{
+			number = Math.sqrt(num);
+		}
+		//or else call sqrt method of Math class and return the value
+
+		return number;
+	}
 
 	public static void main(String[] args) {
 		ScientificCalculator scientificCalculator = new ScientificCalculator();
+		//Call all the methods one by one and surround them by try and respective catch blocks.
 		try {
 			scientificCalculator.cielOfANumber(10);
+			scientificCalculator.power(10,20);
+			scientificCalculator.floorOfANumber(23.56f);
+			scientificCalculator.squareRoot(23);
 		} catch (CalculatorException e) {
-			throw new RuntimeException(e);
+
+			//throw new RuntimeException(e);
+			System.out.println("e = " + e);
+
 		}
-		//Call all the methods one by one and surround them by try and respective catch blocks.
+
 	}
 }
